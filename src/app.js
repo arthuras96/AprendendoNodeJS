ec3480b53dc8a477f2b5e3f9ee1704617381eddb
Arthur Alencar <arthur.alencar@strong.com.br>
2019-05-03 15:38:37 -0300

    First commit.

---
+++ src/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+
+//Conecta ao banco
+mongoose.connect('mongodb://arthur:arthur22@ds021994.mlab.com:21994/nodestr');
+
+//Carrega os modelos
+const Product = require('./models/product');
+
+//Carrega rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
